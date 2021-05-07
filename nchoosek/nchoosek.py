75fbc89d39a9227f68d751c820cedafbd63bb4f4
Scott Pakin <pakin@lanl.gov>
2021-05-07 16:22:04 -0600

    Represent constraints in terms of Constraint objects

--- nchoosek/nchoosek.py
+++ nchoosek/nchoosek.py
@@ -19,6 +19,13 @@ class DuplicatePortError(Exception):
     def __init__(self, port_name):
         super().__init__('Port "%s" already exists in the environment' % port_name)
 
+class Constraint(object):
+    'Representation of a constraint (k of n ports are True).'
+
+    def __init__(self, port_list, num_true):
+        self.port_list = port_list     # List of ports, can include duplicates
+        self.num_true = set(num_true)  # Set of allowable True counts
+
 class Block(object):
     'Base class for user-defined NchooseK types.'
 
@@ -29,32 +36,34 @@ class Block(object):
         env._next_id += 1
 
         # Notify our parent environment of our constraints and our port names.
-        for lps, vals in self.type_constraints:
+        if self.type_constraint != None:
+            lps = self.type_constraint.port_list
+            vals = self.type_constraint.num_true
             gps = ['%s.%s' % (self._unique_id, lp) for lp in lps]
             gps_set = set(gps)
             dups = env._port_names & gps_set
             if len(dups) > 0:
                 raise DuplicatePortError(dups.pop())
             env._port_names |= gps_set
-            env._constraints.append((gps, vals))
+            env._constraints.append(Constraint(gps, vals))
 
         # If a list of port bindings was provided, equate those to the
         # global port names.
         if bindings != None:
-            if len(bindings) != len(self.port_names):
-                raise ValueError('%d binding(s) were provided for %d port(s)' % (len(bindings), len(self.port_names)))
-            for gp1, gp2 in zip(bindings, [self[p] for p in self.port_names]):
+            if len(bindings) != len(self.port_list):
+                raise ValueError('%d binding(s) were provided for %d port(s)' % (len(bindings), len(self.port_list)))
+            for gp1, gp2 in zip(bindings, [self[p] for p in self.port_list]):
                 env.same(gp1, gp2)
 
     def __getattr__(self, attr):
         'Given a type-local port name, return an environment-global port name.'
-        if attr in self.port_names:
+        if attr in self.port_list:
             return '%s.%s' % (self._unique_id, attr)
         raise AttributeError(attr)
 
     def __getitem__(self, key):
         'Given a type-local port name, return an environment-global port name.'
-        if key in self.port_names:
+        if key in self.port_list:
             return '%s.%s' % (self._unique_id, key)
         raise KeyError(key)
 
@@ -68,27 +77,27 @@ class Env(object):
         self._next_id = 1         # Next available unique ID for an object
 
     def new_port(self, port_name):
-        'Define a new, top-level port name.'
+        'Define a new, environment-global port name.  Return the name unmodified.'
         if port_name in self._port_names:
             raise DuplicatePortError(port_name)
         self._port_names.add(port_name)
         return port_name
 
-    def new_type(self, name, port_names, constraints=[]):
+    def new_type(self, name, port_list, constraint=None):
         '''Define a new data type, characterized by a type name, a set of
         type-local port names, and a list of constraints.'''
         # Ensure that all constraints reference only known port names.
-        port_set = set(port_names)
-        for lps, vals in constraints:
-            for lp in lps:
-                if lp not in port_names:
+        port_set = set(port_list)
+        if constraint != None:
+            for lp in constraint.port_list:
+                if lp not in port_set:
                     raise UnknownPortError(name, lp)
 
         # Derive a type from Block and return it.
         return type(name, (Block,), {
             '_type_name': name,
-            'port_names': list(port_names),
-            'type_constraints': constraints,
+            'port_list': list(port_list),
+            'type_constraint': constraint,
             'env': self})
 
     def same(self, gp1, gp2):
@@ -97,7 +106,7 @@ class Env(object):
             raise UnknownPortError(None, gp1)
         if gp2 not in self._port_names:
             raise UnknownPortError(None, gp2)
-        self._constraints.append((set([gp1, gp2]), [0, 2]))
+        self._constraints.append(Constraint([gp1, gp2], {0, 2}))
 
     def different(self, gp1, gp2):
         'Declare that two environment-global ports must have different values.'
@@ -105,7 +114,7 @@ class Env(object):
             raise UnknownPortError(None, gp1)
         if gp2 not in self._port_names:
             raise UnknownPortError(None, gp2)
-        self._constraints.append((set([gp1, gp2]), [1]))
+        self._constraints.append(Constraint([gp1, gp2], {1}))
 
     def nck(self, gps, vals):
         '''Add a new constraint to the environment.  This method accepts
@@ -113,4 +122,4 @@ class Env(object):
         for gp in gps:
             if gp not in self._port_names:
                 raise UnknownPortError(None, gp)
-        self._constraints.append((gps, vals))
+        self._constraints.append(Constraint(gps, vals))
