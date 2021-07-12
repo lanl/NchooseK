cca9f648a4988609c9b557ea8e77441b8f645ac4
Scott Pakin <pakin@lanl.gov>
2021-07-12 17:21:43 -0600

    Provide an example of performing integer addition using NchooseK

---
+++ examples/addition.py
@@ -0,0 +1,76 @@
+#! /usr/bin/env python
+
+################################################
+# Demonstrate that NchooseK can be used to add #
+# integers (although not particularly easily)  #
+#                                              #
+# By Scott Pakin <pakin@lanl.gov>              #
+################################################
+
+import argparse
+import nchoosek
+from nchoosek.solve import z3
+import sys
+
+# Parse the command line.
+parser = argparse.ArgumentParser(description='Add two numbers using NchooseK.')
+parser.add_argument('augend', type=int, metavar='NUM1',
+                    help='First number to add')
+parser.add_argument('addend', type=int, metavar='NUM2',
+                    help='Second number to add')
+parser.add_argument('--bits', '-b', type=int, metavar='INT',
+                    help='Number of bits to use for each input')
+cl_args = parser.parse_args()
+num1 = cl_args.augend
+num2 = cl_args.addend
+if num1 < 0 or num2 < 0:
+    sys.exit('%s: negative numbers are not yet supported' % sys.argv[0])
+nbits = cl_args.bits
+if nbits == None:
+    nbits = 1
+    mval = max(num1, num2) >> 1
+    while mval > 0:
+        nbits += 1
+        mval >>= 1
+if num1 >= 2**nbits:
+    sys.exit('%s: the number %d cannot be represented with %d bit(s)' % (sys.argv[0], num1, nbits))
+if num2 >= 2**nbits:
+    sys.exit('%s: the number %d cannot be represented with %d bit(s)' % (sys.argv[0], num2, nbits))
+
+# Create an environment.
+env = nchoosek.Environment()
+
+# Define a full adder.  The tt2nck helper program was used to construct this
+# primitive.
+Adder = env.new_type('adder',
+                     ['carry-in', 'A', 'B', 'C1', 'C0'],
+                     nchoosek.Constraint(['carry-in', 'A', 'B', *2*['C1'], *3*['C0']], {0, 4, 8}))
+
+# Define one port per input bit, output bit, and carry bit.
+a = [env.register_port('A%d' % i) for i in range(nbits)]
+b = [env.register_port('B%d' % i) for i in range(nbits)]
+c = [env.register_port('C%d' % i) for i in range(nbits + 1)]
+cout = [env.register_port('cout%d' % i) for i in range(nbits)]
+
+# Assign a value to each input bit.
+for i in range(nbits):
+    env.nck([a[i]], {(num1>>i)&1})
+    env.nck([b[i]], {(num2>>i)&1})
+
+# Define our carry-in to be initially False then the previous carry-out.
+always_false = env.register_port('false')
+env.nck([always_false], {0})
+cin = [always_false] + cout  # Here we can alias each input with the previous output.
+
+# Add from right to left with carry.
+for i in range(nbits):
+    Adder([cin[i], a[i], b[i], cout[i], c[i]])
+env.same(c[nbits], cout[nbits - 1])
+
+# Solve for the sum, and output it as an integer.
+soln = z3.solve(env)
+num3 = 0
+for i in range(nbits, -1, -1):
+    num3 <<= 1
+    num3 += int(soln['C%d' % i])
+print('%d + %d = %d' % (num1, num2, num3))
