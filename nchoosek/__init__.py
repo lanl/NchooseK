e503cac62394c7e98858e34273b784e6ac7a1c60
Scott Pakin <pakin@lanl.gov>
2021-11-08 16:07:26 -0700

    Define a function that maps a solver name to a solve function

--- nchoosek/__init__.py
+++ nchoosek/__init__.py
@@ -1,20 +1,27 @@
 # Load the core NchooseK functionality.
 from nchoosek.core import *
+import os
+
+
+def _name_to_solver(name):
+    '''Map a solver name to an appropriate solve function.  Raise a ValueError
+    if the name is not recognized.'''
+    if name == 'z3':
+        import nchoosek.solver.z3
+        return nchoosek.solver.z3.solve
+    elif name == 'ocean':
+        import nchoosek.solver.ocean
+        return nchoosek.solver.ocean.solve
+    elif name == 'qiskit':
+        import nchoosek.solver.qiskit
+        return nchoosek.solver.qiskit.solve
+    else:
+        raise ValueError('"%s" is not a recognized NchooseK solver' % name)
+
 
 # Load a solver based on the setting of the NCHOOSEK_SOLVER
 # environment variable
-import os
-import sys
 _solver_name = os.getenv('NCHOOSEK_SOLVER')
-if _solver_name is None or _solver_name == 'z3':
-    import nchoosek.solver.z3
-    solve = nchoosek.solver.z3.solve
-elif _solver_name == 'ocean':
-    import nchoosek.solver.ocean
-    solve = nchoosek.solver.ocean.solve
-elif _solver_name == 'qiskit':
-    import nchoosek.solver.qiskit
-    solve = nchoosek.solver.qiskit.solve
-else:
-    raise ValueError('"%s" (from NCHOOSEK_SOLVER) is not a recognized '
-                     'NchooseK solver' % _solver_name)
+if _solver_name is None:
+    _solver_name = 'z3'
+solve = _name_to_solver(_solver_name)
