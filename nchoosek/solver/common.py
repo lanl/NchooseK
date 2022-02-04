1bbba4c9a2d42c8c118face7a646d0b2c4e7b30d
Scott Pakin <pakin@lanl.gov>
2022-02-03 18:46:11 -0700

    :bug: Rename ancillae across separate sub-QUBOs

--- nchoosek/solver/common.py
+++ nchoosek/solver/common.py
@@ -14,6 +14,14 @@ class ConstraintConversionError(Exception):
         super().__init__(msg)
 
 
+def rename_ancilla(name, inc):
+    'Rename _ancN with _anc(N+inc).'
+    if name[:4] != '_anc':
+        return name
+    num = int(name[4:])
+    return '_anc%d' % (num + inc)
+
+
 def construct_qubo(env, hard_scale):
     'Convert an entire environment to a QUBO.'
     # Scale the weight of hard constraints by either a user-specified
@@ -28,6 +36,7 @@ def construct_qubo(env, hard_scale):
 
     # Merge all constraints into a single, large QUBO.
     qubo = defaultdict(lambda: 0)
+    total_anc = 0   # Total number of ancillae across all constraints
     for c in env.constraints():
         qqv, _ = c.solve_qubo()
         if qqv is None:
@@ -35,5 +44,8 @@ def construct_qubo(env, hard_scale):
         for q1, q2, val in qqv:
             if not c.soft:
                 val *= hard_scale
+            q1 = rename_ancilla(q1, total_anc)
+            q2 = rename_ancilla(q2, total_anc)
             qubo[(q1, q2)] += val
+        total_anc += len({a for a in [q1, q2] if a[:4] == '_anc'})
     return qubo
