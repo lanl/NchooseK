297b9952bf8f6d152ca331db9db541fd54d45b8f
Ellis <ejwilso2@ncsu.edu>
2022-04-29 16:03:57 -0400

    clean up

--- nchoosek/solver/common.py
+++ nchoosek/solver/common.py
@@ -82,34 +82,31 @@ class Result(object):
             ret += "times: " + str(self.times) + "\n"
         if self.quantum_instance:
             ret += "Qiskit backend: " + str(self.quantum_instance.backend) + "\n"
-            # ret += "  backend: " + str(self.qiskit_info['backend']) + "\n"
-            # ret += "  group: " + str(self.qiskit_info['group']) + "\n"
-            # ret += "  : " + str(self.qiskit_info['group']) + "\n"
         ret = ret[:-1]
         return ret
 
     def details(self):
         if self.quantum_instance:
-            # try:
-            device = self.quantum_instance.backend
-            print(device)
-            print(self.times[0])
-            print(self.times[1])
-            jobs = device.jobs(limit=50, start_datetime=self.times[0], end_datetime=self.times[1])
-            print(jobs)
-            qasm = jobs[2].circuits()[0].qasm()
-            print(qasm)
-            count = 0
-            # Qiskit jobs don't tell you how many physical qubits get used;
-            # we need to search through the final qasm.
-            for i in range(device.configuration().n_qubits):
-                if re.search(r"cx[^;]*q\[" + str(i) + r"\]", qasm) or re.search(r"rz\([^\(]*\) q\[" + str(i) + r"\]", qasm):
-                    count += 1
+            try:
+                device = self.quantum_instance.backend
+                print(device)
+                print(self.times[0])
+                print(self.times[1])
+                jobs = device.jobs(limit=50, start_datetime=self.times[0], end_datetime=self.times[1])
+                print(jobs)
+                qasm = jobs[2].circuits()[0].qasm()
+                print(qasm)
+                count = 0
+                # Qiskit jobs don't tell you how many physical qubits get used;
+                # we need to search through the final qasm.
+                for i in range(device.configuration().n_qubits):
+                    if re.search(r"cx[^;]*q\[" + str(i) + r"\]", qasm) or re.search(r"rz\([^\(]*\) q\[" + str(i) + r"\]", qasm):
+                        count += 1
 
-            self.jobIDs = []
-            for job in jobs:
-                self.jobIDs.append(job.job_id())
-            self.qubits = count
-            self.depth = jobs[2].circuits()[0].depth()
-            # except:
-                # print("Jobs not found")
+                self.jobIDs = []
+                for job in jobs:
+                    self.jobIDs.append(job.job_id())
+                self.qubits = count
+                self.depth = jobs[2].circuits()[0].depth()
+            except:
+                pass
